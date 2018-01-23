1d1a225f06f18183f02e56d2dbff74ab5306d74b
Sheila Viana <sheila.shviana@gmail.com>
2018-01-23 16:35:37 -0200

    Adicionando novos testes

--- src/test/java/br/com/caelum/ingresso/model/descontos/DescontoTest.java
+++ src/test/java/br/com/caelum/ingresso/model/descontos/DescontoTest.java
@@ -5,6 +5,7 @@ import java.time.Duration;
 import java.time.LocalTime;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import br.com.caelum.ingresso.model.Filme;
@@ -13,18 +14,41 @@ import br.com.caelum.ingresso.model.Sala;
 import br.com.caelum.ingresso.model.Sessao;
 
 public class DescontoTest {
+	
+	private Sala sala;
+	private Filme filme;
+	private Sessao sessao;
+	
+	@Before
+	public void preparaSessoes(){
+	
+		sala = new Sala("Eldorado - IMAX", new BigDecimal("20.5"));
+		filme = new Filme("Rogue One", Duration.ofMinutes(120), "SCI-FI", new BigDecimal("12.0"));
+		sessao = new Sessao(LocalTime.parse("10:00:00"), filme, sala);
+	}
 
 	@Test
 	public void naoDeveConcederDescontoParaIngressoNormal(){
-
-		Sala sala = new Sala("Eldorado - IMAX", new BigDecimal("22.5"));
-		Filme filme = new Filme("Rogue One", Duration.ofMinutes(120), "SCI-FI", new BigDecimal("12.0"));
-	
-		Sessao sessao = new Sessao(LocalTime.parse("10:00:00"), filme, sala);
+		
 		Ingresso ingresso = new Ingresso(sessao, new SemDesconto());
-		BigDecimal precoEsperado = new BigDecimal("34.5");
+		BigDecimal precoEsperado = new BigDecimal("32.5");
 	    Assert.assertEquals(precoEsperado, ingresso.getPreco());
 	    //DENTRO DO ASSERT - primeiro valor esperado depois o valor que possuo	
+	}
+	
+    @Test
+	public void deveConcederDescontoDe30PorcentoParaIngressosDeClientesDeBancos() {
+		
+		Ingresso ingresso = new Ingresso(sessao, new DescontoParaBancos());
+		BigDecimal precoEsperado = new BigDecimal("22.75");
+		Assert.assertEquals(precoEsperado, ingresso.getPreco());
+	}
     
+	@Test
+	public void deveConcederDescontoDe50PorcentoParaIngressoDeEstudante() {
+		
+		Ingresso ingresso = new Ingresso(sessao, new DescontoParaEstudantes());
+		BigDecimal precoEsperado = new BigDecimal("16.25");
+		Assert.assertEquals(precoEsperado, ingresso.getPreco());
 	}
 }
